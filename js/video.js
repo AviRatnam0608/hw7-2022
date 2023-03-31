58322548d6f5470a2abcb2b7dcfb35a8433ce908
Avinash Ratnam <avinashr@umich.edu>
2023-03-30 21:49:16 -0400

    Done

--- js/video.js
+++ js/video.js
@@ -1,7 +1,7 @@
 const video = document.querySelector("#player1");
 
 window.addEventListener("load", function () {
-  console.log("Good job opening the window");
+  video.autoplay = false;
 });
 
 document.querySelector("#play").addEventListener("click", function () {
@@ -13,5 +13,49 @@ document.querySelector("#pause").addEventListener("click", function () {
 });
 
 document.querySelector("#slower").addEventListener("click", function () {
-  video.slowDown *= 0.95;
+  video.playbackRate -= 0.05;
+  console.log("Current playbackRate:", video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function () {
+  video.playbackRate += 0.05;
+  console.log("Current playbackRate:", video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function () {
+  if (video.currentTime + 15 <= video.duration) {
+    video.currentTime += 15;
+  } else {
+    video.currentTime = 0;
+    video.pause();
+  }
+  console.log("Current Timestamp:", video.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function () {
+  let muteText = document.querySelector("#mute");
+  switch (muteText.textContent) {
+    case "Mute":
+      muteText.textContent = "Unmute";
+      video.muted = true;
+      break;
+    case "Unmute":
+      muteText.textContent = "Mute";
+      video.muted = false;
+      break;
+    default:
+      break;
+  }
+});
+
+document.querySelector("#slider").addEventListener("change", function () {
+  video.volume = this.value / 100;
+});
+
+document.querySelector("#vintage").addEventListener("click", function () {
+  video.className = "oldSchool";
+});
+
+document.querySelector("#orig").addEventListener("click", function () {
+  video.className = "";
 });
